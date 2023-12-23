a4bfa242eea114533c71ee886c6f0b69c0713217
Ayanda Vilakazi <ayandaxjr@icloud.com>
2023-12-23 17:03:44 +0200

    final commit.

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -35,15 +35,33 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>The Complete 2023 Web Development Bootcamp</li>
-        <li>The Creative JavaScript Course</li>
-        <li>10 Mega Responsive Websites with HTML, CSS and JavaScript</li>
-        <li>Combinatorics (University Level): Counting Principle</li>
-        <li>Discrete Math Series : Propositional Logic masterclass</li>
-        <li>Discrete Math Series : Set Theory & Algebra masterclass</li>
-        <li>Complete Mathematics Masterclass: College & University Level</li>
-        <li>Sets, Relations, Functions & Discrete Mathematical Induction</li>
-        <li>Discrete Mathematics</li>
+        <a href="/images/2023 COMPLETE WEB DEVELOPMENT BOOTCAMP.pdf" download>
+          <li>The Complete 2023 Web Development Bootcamp</li>
+        </a>
+        <a href="/images/CREATIVE JAVASCRIPT.pdf" download>
+          <li>The Creative JavaScript Course</li>
+        </a>
+        <a href="/images/RESPONSIVE WEBSITES.pdf" download>
+          <li>10 Mega Responsive Websites with HTML, CSS and JavaScript</li>
+        </a>
+        <a href="/images/2023 COMPLETE WEB DEVELOPMENT BOOTCAMP.pdf" download>
+          <li>Combinatorics (University Level): Counting Principle</li>
+        </a>
+        <a href="/images/PROPOSITIONAL LOGIC MASTERCLASS.pdf" download>
+          <li>Discrete Math Series : Propositional Logic masterclass</li>
+        </a>
+        <a href="/images/SET THEORY & ALGEBRA MASTERCLASS.pdf" download>
+          <li>Discrete Math Series : Set Theory & Algebra masterclass</li>
+        </a>
+        <a href="/images/COMPLETE MATHEMATICS COLLEGE AND UNIVERSITY LEVEL.pdf" download>
+          <li>Complete Mathematics Masterclass: College & University Level</li>
+        </a>
+        <a href="/images/DISCRETE MATHEMATICAL INDUCTION.pdf" download>
+          <li>Sets, Relations, Functions & Discrete Mathematical Induction</li>
+        </a>
+        <a href="/images/DISCRETE MATHEMATICS.pdf" download>
+          <li>Discrete Mathematics</li>
+        </a>
       </ul>
     ),
   },
@@ -71,7 +89,9 @@ const AboutSection = () => {
             working with JavaScript, React, NEXTjs 13, Node.js, Express, Microsoft Access Database Management,
             MONGODB, PRISMA DB, HTML, CSS and TAILWIND CSS. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications and awesome browsing experiences for users.
+            I am excited to work with others to create amazing applications and awesome browsing experiences for users. <br />
+            Please note to be able to download the certificates, simply click on top of the name of the desired certificate
+            to begin the download to view the certificate on your device.
           </p>
           <div className="flex flex-row justify-center mt-8">
             <TabButton
